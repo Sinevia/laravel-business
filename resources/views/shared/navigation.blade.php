6dd234bbc929e3248fb9cf720e3eca66d102c431
Sinevia <info@sinevia.com>
2019-07-02 00:50:02 +0000

    Updates

--- resources/views/shared/navigation.blade.php
+++ resources/views/shared/navigation.blade.php
@@ -1,21 +1,21 @@
 <?php
-$transactionCount = \Sinevia\Accounting\Models\Transaction::count();
-$invoicesCount = \Sinevia\Accounting\Models\Invoice::where('Status', '<>', 'Deleted')->count();
+$transactionCount = \Sinevia\Business\Models\Transaction::count();
+$invoicesCount = \Sinevia\Business\Models\Invoice::where('Status', '<>', 'Deleted')->count();
 ?>
 <div class="panel panel-default">
     <div class="panel-body" style="padding: 2px;">
         <ul class="nav nav-pills">
             <li>
-                <a href="<?php echo \Sinevia\Accounting\Helpers\Links::adminHome(); ?>">Dashboard</a>
+                <a href="<?php echo \Sinevia\Business\Helpers\Links::adminHome(); ?>">Dashboard</a>
             </li>
             <li>
-                <a href="<?php echo \Sinevia\Accounting\Helpers\Links::adminTransactionManager(); ?>">
+                <a href="<?php echo \Sinevia\Business\Helpers\Links::adminTransactionManager(); ?>">
                     Transactions
                     <span class="badge"><?php echo $transactionCount; ?></span>
                 </a>
             </li>
             <li>
-                <a href="<?php echo \Sinevia\Accounting\Helpers\Links::adminInvoiceManager(); ?>">
+                <a href="<?php echo \Sinevia\Business\Helpers\Links::adminInvoiceManager(); ?>">
                     Invoices
                     <span class="badge"><?php echo $invoicesCount; ?></span>
                 </a>
